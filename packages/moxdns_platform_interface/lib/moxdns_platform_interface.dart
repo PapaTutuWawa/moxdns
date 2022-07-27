16f22dc4ffed556c459444800142e6fea0db8a9d
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2022-07-27 22:34:44 +0200

    style: Migrate to very_good_analysis

--- packages/moxdns_platform_interface/lib/moxdns_platform_interface.dart
+++ packages/moxdns_platform_interface/lib/moxdns_platform_interface.dart
@@ -1,23 +1,23 @@
-import "package:plugin_platform_interface/plugin_platform_interface.dart";
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 class SrvRecord {
-  final String target;
-  final int port;
-  final int priority;
-  final int weight;
 
   const SrvRecord(
     this.target, 
     this.port,
     this.priority,
-    this.weight
+    this.weight,
   );
+  final String target;
+  final int port;
+  final int priority;
+  final int weight;
 }
 
 abstract class MoxdnsPlatform extends PlatformInterface {
-  static final Object _token = Object();
 
   MoxdnsPlatform() : super(token: _token);
+  static final Object _token = Object();
 
   /// Perform the SRV query for [domain]. Use DNSSEC if [dnssec] is true.
   Future<List<SrvRecord>> srvQuery(String domain, bool dnssec);
