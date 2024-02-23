3de4c1c5ac0cc91812b789a14d5a399cf0be18b4
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2024-02-23 19:10:04 +0100

    meta: Bump flake

--- flake.nix
+++ flake.nix
@@ -3,34 +3,50 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    android-nixpkgs.url = "github:tadfisher/android-nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let
+  outputs = { self, nixpkgs, flake-utils, android-nixpkgs }: flake-utils.lib.eachDefaultSystem (system: let
     pkgs = import nixpkgs {
       inherit system;
-      config.android_sdk.accept_license = true;
+      config = {
+        android_sdk.accept_license = true;
+        allowUnfree = true;
+
+        # Fix to allow building the NDK package
+        # TODO: Remove once https://github.com/tadfisher/android-nixpkgs/issues/62 is resolved
+        permittedInsecurePackages = [
+          "python-2.7.18.6"
+        ];
+      };
     };
-    android = pkgs.androidenv.composeAndroidPackages {
-      # TODO: Find a way to pin these
-      #toolsVersion = "26.1.1";
-      #platformToolsVersion = "31.0.3";
-      #buildToolsVersions = [ "31.0.0" ];
-      #includeEmulator = true;
-      #emulatorVersion = "30.6.3";
-      platformVersions = [ "28" ];
-      includeSources = false;
-      includeSystemImages = true;
-      systemImageTypes = [ "default" ];
-      abiVersions = [ "x86_64" ];
-      includeNDK = false;
-      useGoogleAPIs = false;
-      useGoogleTVAddOns = false;
-    };
-    pinnedJDK = pkgs.jdk;
+    # Everything to make Flutter happy
+    sdk = android-nixpkgs.sdk.${system} (sdkPkgs: with sdkPkgs; [
+      cmdline-tools-latest
+      build-tools-30-0-3
+      build-tools-33-0-2
+      build-tools-34-0-0
+      platform-tools
+      emulator
+      #patcher-v4
+      platforms-android-28
+      platforms-android-29
+      platforms-android-30
+      platforms-android-31
+      platforms-android-33
+
+      # For flutter_zxing
+      cmake-3-18-1
+      ndk-21-4-7075529
+      # (ndk-21-4-7075529.overrideAttrs (old: {
+      #    buildInputs = old.buildInputs ++ [ pkgs.python27 ];
+      # }))
+    ]);
+    pinnedJDK = pkgs.jdk17;
   in {
     devShell = pkgs.mkShell {
       buildInputs = with pkgs; [
-        flutter pinnedJDK android.platform-tools dart # Flutter
+        flutter pinnedJDK dart sdk # Flutter
 	      gitlint jq # Code hygiene
 	      ripgrep # General utilities
 
@@ -56,9 +72,13 @@
       CPATH = "${pkgs.xorg.libX11.dev}/include:${pkgs.xorg.xorgproto}/include";
       LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [ atk cairo epoxy gdk-pixbuf glib gtk3 harfbuzz pango ];
 
-      ANDROID_HOME = (toString ./.) + "/.android/sdk";
+      ANDROID_SDK_ROOT = "${sdk}/share/android-sdk";
+      ANDROID_HOME = "${sdk}/share/android-sdk";
       JAVA_HOME = pinnedJDK;
-      ANDROID_AVD_HOME = (toString ./.) + "/.android/avd";
+
+      # Fix an issue with Flutter using an older version of aapt2, which does not know
+      # an used parameter.
+      GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${sdk}/share/android-sdk/build-tools/34.0.0/aapt2";
     };
   });
 }
